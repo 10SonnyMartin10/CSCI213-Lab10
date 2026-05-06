425020e1e946cba5470f02d7b8a764e7be5aed6f
Sonny Martin <sonnymartin@go.rmc.edu>
2026-05-06 14:16:57 -0400

    intial version of CousreInfo

---
+++ CourseInfo.java
@@ -0,0 +1,13 @@
+public static void main(String [] args) { 
+	String courseName = "Object-Oreinted Programming"; 
+	String instructor = "Dr. Elouni"; 
+	String semester = "Spring 2026"; 
+	int credits = 4; 
+	String classroom = "COP 204"; 
+	
+	System.out.println("Course Name: " + courseName); 
+	System.out.println("Instructor: " + instructor); 
+	System.out.println("Semester: " + semseter); 
+	System.out.println("Credits: " + credits); 
+	System.out.println("Classroom: " + classroom); 
+} 
