9b60ebcab7be6543b1b87586a0b24b53a05f6622
Sonny Martin <sonnymartin@go.rmc.edu>
2026-05-07 13:40:05 -0400

    added class time and students

--- CourseInfo.java
+++ CourseInfo.java
@@ -4,10 +4,14 @@ public static void main(String [] args) {
 	String semester = "Spring 2026"; 
 	int credits = 4; 
 	String classroom = "COP 204"; 
+	String classTime = "MW 1:10-2:40 TR 1:20-2:50"; 
+	int numStudents = 23; 
 	
 	System.out.println("Course Name: " + courseName); 
 	System.out.println("Instructor: " + instructor); 
-	System.out.println("Semester: " + semseter); 
+	System.out.println("Semester: " + semester); 
 	System.out.println("Credits: " + credits); 
 	System.out.println("Classroom: " + classroom); 
+	System.out.println("Class Time: " + classTime); 
+	System.out.println("Number of Students: " + numStudents); 
 } 
